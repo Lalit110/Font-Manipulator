1bb9681f64a2b22427e1fe7841a8b10983c4a9ca
Lalit110 <83270963+Lalit110@users.noreply.github.com>
2022-06-10 17:13:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550, 450);
+    canvas.position(560, 150);
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+
+function draw(){
+    background("#ff0303");
+}
+
+function modelLoaded(){
+    console.log("PoseNet Is Initialized");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
